571b7f92c7430ecc1f98fb9f8f9566122f8edc0e
Ilippy <Ilippyk@gmail.com>
2017-04-27 15:47:00 +0300

    Initial commit

---
+++ src/AdvanceInterface/ChatWindow.java
@@ -0,0 +1,124 @@
+package AdvanceInterface;
+
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.*;
+//import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.DefaultCaret;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
+
+public class ChatWindow extends JFrame{
+
+    private JPanel contentPane;
+    private JTextField txtMessage;
+    private JTextArea history;
+    private DefaultCaret caret;
+    private static final String CHAT_CLIENT = "Chat Client";
+    private static String fileName = "src/AdvanceInterface/history.txt";
+
+
+    public ChatWindow()  {
+        creatingWindow();
+        try {
+            history.setText(Files.read(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void creatingWindow() {
+        /*try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }*/
+        setTitle(CHAT_CLIENT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(880, 570);
+        setLocationRelativeTo(null);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+        GridBagLayout gbl_contentPane = new GridBagLayout();
+        gbl_contentPane.columnWidths = new int[]{28, 815, 30, 7}; // SUM = 880
+        gbl_contentPane.rowHeights = new int[]{35, 475, 40}; // SUM = 550
+        gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
+        gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+        contentPane.setLayout(gbl_contentPane);
+
+        history = new JTextArea();
+        history.setEditable(false);
+        JScrollPane scroll = new JScrollPane(history);
+        /*caret = (DefaultCaret) history.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE); */
+        GridBagConstraints scrollConstraints = new GridBagConstraints();
+        scrollConstraints.insets = new Insets(0, 0, 5, 5);
+        scrollConstraints.fill = GridBagConstraints.BOTH;
+        scrollConstraints.gridx = 0;  // выбор столбца из массива {28, 815, 30, 7} (т.е будет 815)
+        scrollConstraints.gridy = 0;  // выбор строки из массива {35, 475, 40} (т.е будет 475)
+        scrollConstraints.gridwidth = 3; //обединяем 3 столбца и ширина столбца будет 28 + 815 + 30
+        scrollConstraints.gridheight = 2; //обединяем 2 строки и высота строки будет 35 + 475
+        scrollConstraints.insets = new Insets(0, 5, 0, 0); // отступы
+        contentPane.add(scroll, scrollConstraints);
+
+        txtMessage = new JTextField();
+        txtMessage.addKeyListener(new KeyAdapter(){
+            public void keyPressed(KeyEvent e){
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                    send(txtMessage.getText());
+                }
+            }
+        });
+        GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+        gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+        gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txtMessage.gridx = 0;
+        gbc_txtMessage.gridy = 2;
+        gbc_txtMessage.gridwidth = 2;
+        contentPane.add(txtMessage, gbc_txtMessage);
+        txtMessage.setColumns(10);
+
+
+        JButton btnSend = new JButton("Send");
+        btnSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                send(txtMessage.getText());
+            }
+        });
+        GridBagConstraints gbc_btnSend = new GridBagConstraints();
+        gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+        gbc_btnSend.gridx = 2;
+        gbc_btnSend.gridy = 2;
+        contentPane.add(btnSend, gbc_btnSend);
+
+        setVisible(true);
+        txtMessage.requestFocusInWindow();
+    }
+
+    private void send(String message){
+        if(message.equals("")) return;
+        console(message);
+        txtMessage.setText("");
+    }
+
+    private void console(String msg){
+        msg += "\n";
+        history.append(msg);
+        history.setCaretPosition(history.getDocument().getLength());
+        try {
+            Files.update(fileName, msg);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/AdvanceInterface/Files.java
@@ -0,0 +1,98 @@
+package AdvanceInterface;
+
+
+import java.io.*;
+
+public class Files {
+    private static String fileName = "src/AdvanceInterface/history.txt";
+    private static File file = new File(fileName);
+
+    private static void write(String fileName, String text) {
+        //Определяем файл
+        File file = new File(fileName);
+
+        try {
+            //проверяем, что если файл не существует то создаем его
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            //PrintWriter обеспечит возможности записи в файл
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+
+            try {
+                //Записываем текст у файл
+                out.print(text);
+            } finally {
+                //После чего мы должны закрыть файл
+                //Иначе файл не запишется
+                out.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String read(String fileName) throws FileNotFoundException {
+        //Этот спец. объект для построения строки
+        StringBuilder sb = new StringBuilder();
+
+        exists(fileName);
+
+        try {
+            //Объект для чтения файла в буфер
+            BufferedReader br = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+            try {
+                //В цикле построчно считываем файл
+                String s;
+                while ((s = br.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            } finally {
+                //Также не забываем закрыть файл
+                br.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Возвращаем полученный текст с файла
+        return sb.toString();
+    }
+
+    private static void exists(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            throw new FileNotFoundException(file.getName());
+        }
+    }
+
+    public static void update(String nameFile, String newText) throws FileNotFoundException {
+        exists(fileName);
+        StringBuilder sb = new StringBuilder();
+        String oldFile = read(nameFile);
+        sb.append(oldFile);
+        sb.append(newText);
+        write(nameFile, sb.toString());
+    }
+
+
+    public static void main(String[] args) {
+        /*write(fileName, "something");
+        write(fileName, "something");
+        write(fileName, "something");
+        write(fileName, "something");*/
+        String textFromFile = null;
+        try {
+            textFromFile = Files.read(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(textFromFile);
+    }
+
+
+}
+
+
